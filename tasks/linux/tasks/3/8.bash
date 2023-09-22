34c83faf592be99455b3c98c3975203a9aace30a
TheOriginalMJKey <fulminant23@gmail.com>
2023-09-22 02:03:57 -0400

    Linux1

--- tasks/linux/tasks/3/8.bash
+++ tasks/linux/tasks/3/8.bash
@@ -1,2 +1,7 @@
 # Разрешите исполнять для владельца, писать для группы и владельца и читать для всех.
-chmod -R a-rwx, g+w, u+wx, a+r folder_name
+
+for files:
+chmod g+w,u+wx,a+r file_name
+ 
+for folders:
+chmod -R g+w,u+wx,a+r folder_name
