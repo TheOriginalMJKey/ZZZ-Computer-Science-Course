12d8182b0d34623ea8c67cebb1396798bdc883fd
TheOriginalMJKey <fulminant23@gmail.com>
2023-10-29 11:09:16 -0400

    integers3

--- tasks/integers/tasks/3/solution.h
+++ tasks/integers/tasks/3/solution.h
@@ -1,9 +1,40 @@
-#pragma once
-
-#include <stdint.h>
 #include <stdio.h>
 
+const int TEN = 10;
+const int ZERO = 0;
+
+int MaxSum(int num) {
+    int maxSum = ZERO;
+    int currentSum = ZERO;
+
+    int firstDigit = num % TEN;
+    num /= TEN;
+    int secondDigit = num % TEN;
+    num /= TEN;
+
+    while (num > ZERO) {
+        int thirdDigit = num % TEN;
+
+        currentSum = firstDigit + secondDigit + thirdDigit;
+        if (currentSum > maxSum) {
+            maxSum = currentSum;
+        }
+
+        firstDigit = secondDigit;
+        secondDigit = thirdDigit;
+        num /= TEN;
+    }
+
+    return maxSum;
+}
+
 int Task() {
-    // write your solution here
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    int maxSum = MaxSum(num);
+    printf("Maximum sum of triplets: %d\n", maxSum);
+
     return 0;
 }
