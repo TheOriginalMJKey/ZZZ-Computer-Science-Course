2d7964792da73603e0fefa37d80d2f27578b4c63
TheOriginalMJKey <fulminant23@gmail.com>
2023-11-08 09:35:49 -0500

    bitset14

--- tasks/bitsets/tasks/14/solution.h
+++ tasks/bitsets/tasks/14/solution.h
@@ -7,14 +7,14 @@ const uint8_t LENGTH = 26;
 
 typedef uint32_t Bitset;
 
-bool is_vowel(char letter) {
+bool isVowel(char letter) {
     if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u' || letter == 'y') {
-        return 1;
+        return true;
     }
-    return 0;
+    return false;
 }
 
-uint32_t count_consonants(Bitset bitset) {
+uint32_t CountConsonants(Bitset bitset) {
     uint32_t checker_counter = 0;
     for (int i = 0; i <= LENGTH; ++i) {
         if (bitset & (1u << i)) {
@@ -27,16 +27,16 @@ uint32_t count_consonants(Bitset bitset) {
 int Task() {
     Bitset bitset = 0;
     char letter = 0;
-    while (1) {
+    while (true) {
         letter = (char)getchar();
         if (letter == EOF) {
             break;
         }
-        if (letter >= 'a' && letter <= 'z' && !is_vowel(letter)) {
+        if (letter >= 'a' && letter <= 'z' && !isVowel(letter)) {
             bitset |= (1u ^ (~(1u & (letter - 'a' + 31) % 32)));
         }
     }
-    uint32_t checker_counter = count_consonants(bitset);
+    uint32_t checker_counter = CountConsonants(bitset);
     if (checker_counter < MAX_CONSONANTS) {
         printf("yes");
     } else {
