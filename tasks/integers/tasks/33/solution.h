da8e084e7f54b24e7f5dc64b0204d5597bdae64b
SashaMaltsev <144154407+SashaMaltsev@users.noreply.github.com>
2024-01-07 08:00:57 +0300

    integers33

--- tasks/integers/tasks/33/solution.h
+++ tasks/integers/tasks/33/solution.h
@@ -3,7 +3,24 @@
 #include <stdint.h>
 #include <stdio.h>
 
+const int32_t BASE = 2;
+
 int Task() {
-    // write your solution here
+
+    int32_t number = 0;
+    int32_t count = 0;
+
+    scanf("%d", &number);
+
+    while (number > 0) {
+        count += (number % BASE ? 1 : -1);
+        number /= BASE;
+    }
+
+    if (count == 0) {
+        printf("Число содержит равные количества нулей и сдиниц в двоичном виде\n");
+    } else {
+        printf("Число не содержит равные количества нулей и сдиниц в двоичном виде\n");
+    }
     return 0;
 }
